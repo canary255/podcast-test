ee859e9d26bde187430e872b50c16f5fe9f8ea35
canary255 <iaarabu@gmail.com>
2022-11-16 20:09:05 +0000

    Add NumberEpisodes component

---
+++ src/Components/atom/NumberEpisodes/index.stories.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { NumberEpisodes } from ".";
+
+export default {
+  title: "Atom/NumberEpisodes",
+  component: NumberEpisodes,
+  argTypes: {},
+};
+
+const Template = (args) => (
+  <NumberEpisodes {...args}>{args.children}</NumberEpisodes>
+);
+
+export const Default = Template.bind({});
+Default.args = {
+  children: 10,
+};
