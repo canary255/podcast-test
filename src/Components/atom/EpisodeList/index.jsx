b9c3e05c0051ba89529a4c974ae955f5a26c4d57
canary255 <iaarabu@gmail.com>
2022-11-16 22:39:21 +0000

    Add EpisodeList

---
+++ src/Components/atom/EpisodeList/index.jsx
@@ -0,0 +1,37 @@
+export const EpisodeList = ({ list }) => {
+  return (
+    <div className="shadow-lg w-full p-4">
+      <table className="w-full text-sm text-left">
+        <thead>
+          <tr>
+            <th scope="col" className="py-3 px-14">
+              Title
+            </th>
+            <th scope="col" className="py-3">
+              Date
+            </th>
+            <th scope="col" className="py-3 text-center">
+              Duration
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          {list.map((item, index) => (
+            <tr
+              key={index}
+              className="first:border-t-4 border-b-2 even:bg-white odd:bg-gray-100"
+            >
+              <td className="py-3 px-14">
+                <a className=" text-blue-500" href="/">
+                  {item.title}
+                </a>
+              </td>
+              <td className="py-3 ">{item.date}</td>
+              <td className="py-3  text-center">{item.duration}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
---
+++ src/Components/atom/EpisodeList/index.stories.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { EpisodeList } from ".";
+
+export default {
+  title: "Atom/EpisodeList",
+  component: EpisodeList,
+  argTypes: {},
+};
+
+const list = [
+  { title: "Darkest hours", date: "15/01/2011", duration: "4:11" },
+  { title: "The Game Never Ends", date: "24/10/2011", duration: "3:53" },
+  { title: "Under Flaming Skies", date: "08/04/2011", duration: "3:51" },
+  { title: "Darkest hours", date: "15/01/2011", duration: "4:11" },
+  { title: "The Game Never Ends", date: "24/10/2011", duration: "3:53" },
+  { title: "Under Flaming Skies", date: "08/04/2011", duration: "3:51" },
+  { title: "Darkest hours", date: "15/01/2011", duration: "4:11" },
+  { title: "The Game Never Ends", date: "24/10/2011", duration: "3:53" },
+  { title: "Under Flaming Skies", date: "08/04/2011", duration: "3:51" },
+];
+
+const Template = (args) => <EpisodeList {...args}>{args.children}</EpisodeList>;
+
+export const Default = Template.bind({});
+Default.args = {
+  list,
+};
