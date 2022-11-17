cdf67c3d6f8306c34dda93eb5de3d47711b1ee6f
canary255 <iaarabu@gmail.com>
2022-11-17 11:41:04 +0000

    Add badge

---
+++ src/Components/atom/Badge/index.jsx
@@ -0,0 +1,7 @@
+export const Badge = ({ children }) => {
+  return (
+    <span class="px-2 rounded-lg text-white bg-blue-500 font-semibold text-lg flex align-center w-max">
+      {children}
+    </span>
+  );
+};
