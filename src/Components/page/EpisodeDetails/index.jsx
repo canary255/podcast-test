afa097306d2618adf72def30788930473f6d93d0
canary255 <iaarabu@gmail.com>
2022-11-17 20:06:48 +0000

    add EpisodeDetails

--- src/Components/page/EpisodeDetails/index.jsx
+++ src/Components/page/EpisodeDetails/index.jsx
@@ -10,8 +10,22 @@ export const EpisodeDetails = () => {
     src: "https://m.media-amazon.com/images/I/610ZK-2PFrL.jpg",
   };
 
+  const episode = {
+    title: "The game never ends",
+    src: "http://themushroomkingdom.net/sounds/wav/sm64/sm64_exit_course_pause_menu.wav",
+  };
+
+  const description = `<div><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+  Sed ex urna, rhoncus vel sapien eget, lacinia iaculis arcu. Nulla convallis dignissim lorem, 
+  egestas tempus ante bibendum aliquam. <b>Etiam tempus mattis scelerisque.</b> 
+  Fusce porttitor consectetur placerat. Proin non blandit risus. 
+  Fusce a arcu nec arcu ultrices dignissim. Aliquam euismod sit amet ipsum posuere elementum. 
+  Praesent sapien augue, varius sed egestas ac, lobortis ac lorem.</p>
+  <a class="text-blue-500" href="http://www.google.com" target="_blank">Donec imperdiet euismod lorem, 
+  non luctus ligula finibus sit amet. Duis auctor purus sit amet efficitur auctor.</a></div>`;
+
   return (
-    <div className="grid grid-rows-3 grid-flow-col gap-4">
+    <div className="grid grid-rows-3 grid-flow-col gap-24">
       <div className="row-span-3 col-span-1">
         <PodcastInfo
           author={podcast.author}
@@ -22,7 +36,11 @@ export const EpisodeDetails = () => {
         </PodcastInfo>
       </div>
       <div className="col-span-2 row-span-2">
-        <EpisodeInfo />
+        <EpisodeInfo
+          title={episode.title}
+          src={episode.src}
+          description={description}
+        />
       </div>
     </div>
   );
