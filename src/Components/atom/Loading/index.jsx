08979ecead1355f7d19fc09c187c148296eba961
canary255 <iaarabu@gmail.com>
2022-11-17 12:34:18 +0000

    Fix loading icon position

--- src/Components/atom/Loading/index.jsx
+++ src/Components/atom/Loading/index.jsx
@@ -1,8 +1,8 @@
 export const Loading = () => {
   return (
-    <span class="flex h-6 w-6">
-      <span class="animate-ping absolute inline-flex h-6 w-6 rounded-full bg-sky-400 opacity-75"></span>
-      <span class="relative inline-flex rounded-full h-6   w-6 bg-sky-500"></span>
+    <span class="flex h-6 w-6 static">
+      <span class="top-5 right-10 animate-ping absolute inline-flex h-6 w-6 rounded-full bg-sky-400 opacity-75"></span>
+      <span class="absolute top-5 right-10 inline-flex rounded-full h-6   w-6 bg-sky-500"></span>
     </span>
   );
 };
