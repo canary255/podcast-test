41f0ea6e2ebf3757322082ac9adb5c2304441e9d
canary255 <iaarabu@gmail.com>
2022-11-21 14:21:01 +0000

    remove unused files, fix typo

--- src/Components/InputFilter/index.jsx
+++ src/Components/InputFilter/index.jsx
@@ -2,7 +2,7 @@ export const InputFilter = ({ onChange }) => {
   return (
     <input
       type="text"
-      id="first_name"
+      id="podcast-filter"
       className="border border-gray-300 text-sm rounded-lg block w-full p-2.5 "
       placeholder="Filter podcasts..."
       onChange={onChange}
